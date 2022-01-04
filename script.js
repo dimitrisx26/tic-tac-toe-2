b82954988c5043fbf7a0701aa50ef7d2f8d5ef22
Dimitris <dxynos@outlook.com>
2022-01-04 16:25:36 +0200

    PvP function has been made

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-const gameBoard = (() => {
-    const boxesArray = [
-        "X","","O",
-        "","X","O",
-        "O","","X"
-    ];
-
-    const blocks = document.querySelectorAll(".block");
-
-    for (let i = 0; i < boxesArray.length; i++) {
-        blocks[i].textContent = boxesArray[i];
-        if (boxesArray[i] !== "") {
-            blocks[i].classList.add("selected");
-        }
-    }
-
-    return {};
-})();
-
-const displayController = (() => {
-
-
-    return {};
-})();
+const blocks = document.querySelectorAll(".block");
 
 const Player = (name, option) => {
     playerName = name;
     playerOption = option;
 
-    return {};
+    return { playerName, playerOption };
 };
+
+const player1 = Player("dimitris", "X");
+const player2 = Player("marlena", "O");
+
+const gameBoard = (() => {
+    const boxesArray = [
+        "","","",
+        "","","",
+        "","",""
+    ];
+
+    return {boxesArray};
+})();
+
+const displayController = (() => {
+    let playerTurn;
+
+    function round ( ) {
+        for (let i = 0; i < gameBoard.boxesArray.length; i++) {
+            blocks[i].addEventListener("click", function () {
+                if (gameBoard.boxesArray[i] == "") {
+                    if (!playerTurn) {
+                        blocks[i].textContent = player1.playerOption;
+                    } else {
+                        blocks[i].textContent = player2.playerOption;
+                    }
+                    blocks[i].classList.add("selected");
+                }
+                swapTurns();
+            })
+        }
+    }
+
+    function swapTurns() {
+        playerTurn = !playerTurn;
+    }
+
+    round();
+
+    return {};
+})();
