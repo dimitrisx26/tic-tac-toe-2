49ff6030606454c9f71fa64075c2a3b2721db293
Dimitris <dxynos@outlook.com>
2022-01-05 20:10:22 +0200

    Finished v1 of the game

--- script.js
+++ script.js
@@ -1,43 +1,119 @@
-const Player = (name, option) => {
-    playerName = name;
-    playerOption = option;
+const startBtn = document.getElementById("startBtn");
 
-    return { playerName, playerOption };
+const Player = (name) => {
+    playerName = name;
+
+    return { playerName };
 };
 
-const player1 = Player("dimitris", "X");
-const player2 = Player("marlena", "O");
+
+const player1 = Player("Player 1");
+const player2 = Player("Player 2");
 
 const gameBoard = (() => {
-    const boxesArray = document.querySelectorAll(".block");
+    
+    const nodesArray = document.querySelectorAll(".block");
+    const announcement = document.getElementById("announcement");
+    const restartBtn = document.getElementById("restartGame");
+    const boxesArray = [];
+    for (let i = 0; i < nodesArray.length; i++) {
+        boxesArray.push(nodesArray[i].textContent);
+    }   
 
-    return {boxesArray};
+    const winningCompinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    return { nodesArray, boxesArray, winningCompinations, announcement, restartBtn };
 })();
 
-const displayController = (() => {
+const game = (() => {
+    let turn;
     let playerTurn;
+    gameBoard.announcement.textContent = `${player1.playerName}'s turn`;
 
-    function round ( ) {
-        for (let i = 0; i < gameBoard.boxesArray.length; i++) {
-            gameBoard.boxesArray[i].addEventListener("click", function () {
-                if (gameBoard.boxesArray[i].textContent === "") {
-                    if (!playerTurn) {
-                        gameBoard.boxesArray[i].textContent = player1.playerOption;
+    (function round() {
+        
+
+        for (let i = 0; i < gameBoard.nodesArray.length; i++) {
+            gameBoard.nodesArray[i].addEventListener("click", function () {
+                gameBoard.announcement.textContent = turn ? `${player1.playerName}'s turn` : `${player2.playerName}'s turn`;
+                if (gameBoard.boxesArray[i] === "") {
+                    if (!turn) {
+                        gameBoard.nodesArray[i].textContent = "X"
+                        gameBoard.boxesArray[i] = "X";
                     } else {
-                        gameBoard.boxesArray[i].textContent = player2.playerOption;
+                        gameBoard.nodesArray[i].textContent = "O"
+                        gameBoard.boxesArray[i] = "O"
                     }
-                    gameBoard.boxesArray[i].classList.add("selected");
+                    gameBoard.nodesArray[i].classList.add("selected");
                 }
-                swapTurns();
+                roundEnd();
             })
+        }
+
+        gameBoard.restartBtn.addEventListener("click", function () {
+            restartRound();
+        })
+    })();
+
+    function restartRound() {
+        turn = false;
+        gameBoard.announcement.textContent = turn ? `${player2.playerName}'s turn` : `${player1.playerName}'s turn`;
+        for (let i = 0; i < gameBoard.nodesArray.length; i++) {
+            gameBoard.nodesArray[i].textContent = ""
+            gameBoard.boxesArray[i] = "";
+            gameBoard.nodesArray[i].classList.remove("selected")
+            gameBoard.nodesArray[i].classList.remove("unclickable");
         }
     }
 
-    function swapTurns() {
-        playerTurn = !playerTurn;
+    function roundEnd() {
+        playerTurn = turn ? "O" : "X";
+        if (checkWin(playerTurn)) {
+            endGame(true);
+        } else if (checkDraw()) {
+            endGame(false);
+        } else {
+            swapTurns();
+        }
     }
 
-    round();
+    function checkWin() {
+        return gameBoard.winningCompinations.some(compination => {
+            return compination.every(index => {
+                return gameBoard.boxesArray[index] === playerTurn;
+            })
+        })
+    }
+
+    function checkDraw() {
+        return gameBoard.boxesArray.every(box => {
+            return box === "X" || box === "O";
+        })
+    }
+
+    function swapTurns() {
+        turn = !turn;
+    }
+
+    function endGame(result) {
+        if (result) {
+            gameBoard.nodesArray.forEach(node => {
+                node.classList.add("unclickable");
+            });
+            gameBoard.announcement.textContent = `${turn ? player2.playerName : player1.playerName} wins`;
+        } else {
+            gameBoard.announcement.textContent = "Tie";
+        }
+    }
 
     return {};
 })();
